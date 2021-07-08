e98b2a89e71a5dc51655162c2076b4455b967d4d
Tejv1708 <80191104+Tejv1708@users.noreply.github.com>
2021-07-08 16:06:04 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,164 @@
+// let featurePoint = [
+//     "check out my Netflix clone   ",
+//     "Here's the code for my project",
+//     "I've just relaunced my portfolio"
+// ]
+// let lists = [
+//     "experience = 2 years" ,
+//     "eduction = 2years",
+//     "license = 1 years",
+// ]
+// //Arrays - ordered lists of items ;
+// console.log[2];
+
+// Array complex data type we store anything in it . 
+
+// let tej = ["Tejveer Singh Naruka " , 21 , true] ;
+// // console.log(tej[2]);
+
+// let cards = [7,4] ;
+
+// cards.push(6);
+// console.log(cards) ;
+
+// let message = [
+//     "hey , how's it going ",
+//     "I'm great , thanku ! How about you",
+//     "All good "
+// ]
+
+// let newmessage = "Same here ";
+// message.push(newmessage);
+// console.log(message);
+
+// message.pop();
+// console.log(message);
+
+// for(let count = 10; count < 21; count+=1 ){
+//     console.log(count)
+// }
+// for(let i =10 ; i<101 ; i+=10){
+//     console.log(i);
+// }
+
+// let message = [
+//    " Hey,How's it going ?",
+//    "i'm great , thanks you ! How about you ",
+//    "All good .Been working on my portfolio lately",
+//    "Same here ",
+
+// ]
+// for(let i=0 ; i<5 ; i++ ){
+//     console.log(message[i]);
+// }
+
+// let cards = [7,3,9] ;
+// for(i=0 ; i<cards.length ; i++) {
+//     console.log(cards[i]);
+
+  
+// } 
+// let sentence = ["hello", "my ", "name " , "is " ,"Tej"]
+// let greetingEl = document.getElementById("greeting-el") ;
+
+// for(let i =0 ; i< sentence.length ; i++){
+//     greetingEl.textContent += sentence[i] ;
+
+//  }
+//  let arrays = [1,2,3,4] ;
+// for(i=0; i< arrays.length; i++) {
+   
+//    console.log(arrays[i]);
+// }
+
+// let card = 3 ;
+
+// arrays.push(card) ;
+// console.log(arrays[i]);
+
+// let player1Time = 102 
+// let player2Time = 107
+
+// function getFasterRaceTime(){
+//     if(player1Time < player2Time) {
+//         return player1Time ;
+//     } else if (player1Time > player2Time){
+// return player2Time ;
+
+//     } else{
+//         return player1Time ;
+//     }
+// }
+
+// function totalRaceTime() {
+//     return player1Time+player2Time ;
+// }
+// let time = totalRaceTime() ;
+// console.log(time) ;
+// let getRandomCard = Math.random()*6 + 1 ;
+
+
+
+// let flooredNumber = Math.floor(getRandomCard)
+
+//  console.log(flooredNumber)
+
+// function rollDice(){
+  
+// let getRandomCard = Math.floor(Math.random()*6) + 1
+//  return getRandomCard ;
+// }
+// let x = rollDice() ;
+// console.log(x) ;
+
+// let hasComoletedCourse = false ;
+//  let hasSolvedChallenge = false ;
+//  let hasHintsleft = false ;
+
+//  if(hasSolvedChallenge === false && hasHintsleft === false){
+//     showSolution()
+//  }
+//  function showSolution() {
+//      console.log("showing this solution.... ") ;
+//  } 
+// let likeDocumentaries = true ;
+// let likeStartUp = true ;
+
+// if(likeDocumentaries === true || likeStartUp === true){
+//     recommendMovies() ;
+// }
+// function recommendMovies() {
+//     console.log("Hey , Check it out ");
+// }
+// let course = {
+//     tittle:"Learn CSS Grid for free " ,
+//     lessons : 16 ,
+//     creator : "Tejveer Singh Naruka " ,
+//     length : 63 ,
+//     level: 2 , 
+//     isFree: true ,
+//     tags: ["html","css"]
+//  }
+
+//  console.log(course.length);
+// let namespace = 12 ; 
+// let  space = 11 ; 
+
+
+
+// let greetingEl  = document.getElementById("greeting-el") ;
+
+// greetingEl.textContent = "Cash :" + "123" ;
+
+
+
+let airhubCastle = {
+
+    tittle : "Live like a King ",
+    Price : 150 ,
+    for2Men : true,
+    images : ["img/castle.png" , "img/castle2.png" ]
+
+}
+console.log(airhubCastle.Price);
+console.log(airhubCastle.images)
